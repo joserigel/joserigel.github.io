394143ebf18bd1d03ade8c4ed3fe7067a8bc7fba
joserigel <joserigel18@gmail.com>
2024-06-07 11:19:56 +0200

    Prepared mobile responsive UI

--- src/modules/pages/Home.tsx
+++ src/modules/pages/Home.tsx
@@ -15,7 +15,7 @@ function Home() {
       <h1 className='name-title'>JOSE <br/>RIGEL</h1>
     </div>
     
-    <img className='waves' src='/waves.svg'/>
+    <img className='waves' src='/waves.svg' alt='waves'/>
 
     <article>
       <div className='card'>
@@ -23,7 +23,7 @@ function Home() {
           Hi, my name is Jose Rigel, a Computer Science Student @ RWTH Aachen University. At the moment, I am on track to finish my studies in 2025 as I'm in my 4<sup>th</sup> Semester.
         </p>
         <p>
-          I'm currently looking for new challenges in Software Engineering, specifically in Working Student positions or Full Time Internships. During my second and third semester of my study, I have worked in the automotive industry as part time Full-Stack Software Developer @ Aptiv.
+          I'm currently looking for new challenges in Software Engineering, specifically in Working Student positions or Full Time Internships. During my second and third semester of my study, I worked in the automotive industry as part time Full-Stack Software Developer.
         </p>
         <p>
           Now I'm currently studying to expand my knowledge in many parts of Computer Science!
@@ -33,15 +33,15 @@ function Home() {
       <div className='card'>
         <ul>
           <li>
-            <img className='icon' src="/icons/gmail.png"/>
+            <img className='icon' src="/icons/gmail.png" alt='gmail'/>
             <a className='contact-button' target="_blank" href="mailto:joserigel18@gmail.com">joserigel18@gmail.com</a>
           </li>
           <li>
-            <img className='icon' src="/icons/LinkedIn.png"/>
+            <img className='icon' src="/icons/LinkedIn.png" alt='LinkedIn'/>
             <a className='contact-button' target="_blank" href="https://www.linkedin.com/in/jose-rigel-26125b243/">joserigel</a>
           </li>
           <li>
-            <img className='icon' src="/icons/github-white.svg"/>
+            <img className='icon' src="/icons/github-white.svg" alt='GitHub'/>
             <a className='contact-button' target="_blank" href="https://github.com/joserigel">joserigel</a>
           </li>
         </ul>
