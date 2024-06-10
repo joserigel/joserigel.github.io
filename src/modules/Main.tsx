d90d4cfe7626bd819ed52124ab887358e1770d25
joserigel <joserigel18@gmail.com>
2024-06-10 23:41:30 +0200

    Finished home page and responsive ui

--- src/modules/Main.tsx
+++ src/modules/Main.tsx
@@ -1,11 +1,45 @@
 import { Outlet } from 'react-router';
 import './Main.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+interface BurgerProps {
+  setExpanded: React.Dispatch<React.SetStateAction<boolean>>;
+  expanded: boolean
+}
+
+function Burger({setExpanded, expanded}: BurgerProps) {
+  return (<div onClick={() => setExpanded((x) => !x)}
+    className={`burger ${expanded ? 'cross' : ''}`}>
+    <div/>
+    <div/>
+    <div/>
+  </div>);
+}
+
 function Main() {
+  const [expanded, setExpanded] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    function onWindowResize(event: any) {
+      if (event.target.innerWidth > 967) {
+        setExpanded(false);
+        setIsMobile(false);
+      } else if (!isMobile) {
+        setTimeout(() => setIsMobile(true), 400);
+      }
+    }
+    window.addEventListener('resize', onWindowResize);
+    onWindowResize({target: {innerWidth: window.innerWidth}});
+
+    return () => window.removeEventListener('resize', onWindowResize)
+  });
+
   return (<>
-    <header>
+    <Burger expanded={expanded} setExpanded={setExpanded}/>
+    <header className={`${expanded ? 'expanded' : ''}`}
+      style={{transitionDuration: isMobile ? '0.2s' : '0s'}}
+    >
         <Link className='nav-item' to='/'>Home</Link>
         <Link className='nav-item' to='project'>Projects</Link>
         <Link className='nav-item' to='blog'>Blog</Link>
--- src/modules/pages/Home.tsx
+++ src/modules/pages/Home.tsx
@@ -11,19 +11,21 @@ function Home() {
       <div className='pink-red-gradient'/>
     </div>
 
-    <div className='icon-position'>
-      <h1 className='name-title'>JOSE <br/>RIGEL</h1>
+    <div className='name-title'>
+      <h1>JOSE</h1>
+      <h1>RIGEL</h1>
     </div>
     
     <img className='waves' src='/waves.svg' alt='waves'/>
 
     <article>
+      <h1 className='name-title-alt'>Hello!</h1>
       <div className='card'>
         <p>
           Hi, my name is Jose Rigel, a Computer Science Student @ RWTH Aachen University. At the moment, I am on track to finish my studies in 2025 as I'm in my 4<sup>th</sup> Semester.
         </p>
         <p>
-          I'm currently looking for new challenges in Software Engineering, specifically in Working Student positions or Full Time Internships. During my second and third semester of my study, I worked in the automotive industry as part time Full-Stack Software Developer.
+          I'm currently looking for new challenges in Software Engineering, specifically in Working Student positions or Full Time Internships. During my second and third semester of my study, I worked in the automotive industry as a part time Full-Stack Software Developer.
         </p>
         <p>
           Now I'm currently studying to expand my knowledge in many parts of Computer Science!
